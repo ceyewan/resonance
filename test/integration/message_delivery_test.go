6a243d7d868004ce3b4f60aa372ba7ec775bbac5
ceyewan <ceyewan@gmail.com>
2026-08-10 10:35:36 +0800

    fix websocket test races

--- test/integration/message_delivery_test.go
+++ test/integration/message_delivery_test.go
@@ -244,19 +244,24 @@ func newWSConnForTest(t *testing.T, username string) (*gwws.Conn, *websocket.Con
 	upgrader := websocket.Upgrader{
 		CheckOrigin: func(r *http.Request) bool { return true },
 	}
-	var serverConn *websocket.Conn
+	serverConnCh := make(chan *websocket.Conn, 1)
 	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		c, err := upgrader.Upgrade(w, r, nil)
 		if err != nil {
 			return
 		}
-		serverConn = c
+		serverConnCh <- c
 	}))
 
 	url := "ws" + strings.TrimPrefix(srv.URL, "http")
 	clientConn, _, err := websocket.DefaultDialer.Dial(url, nil)
 	require.NoError(t, err)
-	require.Eventually(t, func() bool { return serverConn != nil }, 2*time.Second, 20*time.Millisecond)
+	var serverConn *websocket.Conn
+	select {
+	case serverConn = <-serverConnCh:
+	case <-time.After(2 * time.Second):
+		t.Fatal("timed out waiting for server WebSocket connection")
+	}
 
 	conn := gwws.NewConn(
 		username,
